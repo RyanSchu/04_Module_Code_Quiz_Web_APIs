f893db67aa6ae24e5393ab7c962131cd6d5d0e52
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-07-03 18:32:55 -0500

    defined class for question objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,44 @@
+// get the card element which will store the question contents
+var card = document.querySelector(".card")
 
-var card =document.querySelector(".card")
-// var cardHead =document.getElementsByClassName("card-header")
-// var cardContent =document.getElementsByClassName("card-content")
-// var cardFoot =document.getElementsByClassName("card-footer")
+// first lets define a class of objects called questions
+class Question {
+    constructor(title, question, choices, solution, values) {
+        if (!(choices.includes(solution))) {
+            console.log(solution, choices)
+            console.log("invalid input, solution must exist in choices")
+            return
+        }
+        this.title = title
+        this.question = question
+        this.choices = choices
+        this.solution = solution
+        this.values = values
+    }
+}
 
-
+// store questions in objects and objects in an array
 var questions = [
-    {title:"Welcome",question:"",choices:[],solution:""},
-    {title:"Question1",question:"",choices:[],solution:""},
-    {title:"Question2",question:"",choices:[],solution:""},
-    {title:"Question3",question:"",choices:[],solution:""},
-    {title:"Question4",question:"",choices:[],solution:""}
-
+    new Question(title="Question1",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question2",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question3",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question4",question="",choices=["A","B","C","D"],solution="A")
 ]
 
+
+// first we want to initialize the card with a welcome page and start button
+// we could alternatively store the welcome greeting inside the array of questions
+// but i would rather keep it a separate process since a welcome page lacks any kind of content and could have a different solution
 function initializeCard(card) {
+    console.log(card)
     console.log("run")
-    let cardHeader= document.createElement("h2")
+    let cardHeader = document.createElement("h2")
     cardHeader.setAttribute("class","card-header")
     cardHeader.textContent = "Welcome to the quiz"
     card.appendChild(cardHeader)
 }
 
-function renderCard() {
-
-}
-
+initializeCard(card) 
 
 
 
