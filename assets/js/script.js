bf9a919ec76ccc81092a822cbe44dfe67ee50058
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-07-03 19:29:37 -0500

    figured out start card initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// first select relevant elements
 // get the card element which will store the question contents
 var card = document.querySelector(".card")
 
@@ -17,15 +18,6 @@ class Question {
     }
 }
 
-// store questions in objects and objects in an array
-var questions = [
-    new Question(title="Question1",question="",choices=["A","B","C","D"],solution="A"),
-    new Question(title="Question2",question="",choices=["A","B","C","D"],solution="A"),
-    new Question(title="Question3",question="",choices=["A","B","C","D"],solution="A"),
-    new Question(title="Question4",question="",choices=["A","B","C","D"],solution="A")
-]
-
-
 // first we want to initialize the card with a welcome page and start button
 // we could alternatively store the welcome greeting inside the array of questions
 // but i would rather keep it a separate process since a welcome page lacks any kind of content and could have a different solution
@@ -36,7 +28,22 @@ function initializeCard(card) {
     cardHeader.setAttribute("class","card-header")
     cardHeader.textContent = "Welcome to the quiz"
     card.appendChild(cardHeader)
+    let cardButton = document.createElement("button")
+    cardButton.setAttribute("id", "startButton")
+    cardButton.textContent = "Start Quiz"
+    card.appendChild(cardButton)
+
 }
+
+
+// store questions in objects and objects in an array
+var questions = [
+    new Question(title="Question 1/5",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question 2/5",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question 3/5",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question 4/5",question="",choices=["A","B","C","D"],solution="A"),
+    new Question(title="Question 5/5",question="",choices=["A","B","C","D"],solution="A")
+]
 
 initializeCard(card) 
 
