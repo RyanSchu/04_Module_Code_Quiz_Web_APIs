5b2e87fb7ea4f87c2ef13ea54075fa7ca8830e47
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-07-07 16:27:53 -0500

    functions to create timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // first select relevant elements
 // get the card element which will store the question contents
 var card = document.querySelector(".card")
+var timer = document.querySelector(".timer")
 var index = 0
 var startScore = 0
+var timeLeft=80
 // first lets define a class of objects called questions
 class Question {
     constructor(title, question, choices, solution,isStart,buttonContent) {
@@ -33,16 +35,15 @@ class Score {
 // this function adds the content to the page each time the continue button is clicked
 function renderCard(card,question) {
     if(index !== 0 ){
-        // console.log(index)
         card.innerHTML = ''
     }
     renderTitle(card,question)
     renderQuestion(card,question)
     renderChoices(card,question)
-    renderButton(card,question)
+    let timerID = renderButton(card,question)
     card.setAttribute("data-index",index)
     index++
-    return
+    return timerID
 }   
 
 function renderTitle(card,question) {
@@ -83,14 +84,18 @@ function renderButton(card,question) {
     let cardButton = document.createElement("button")
     cardButton.setAttribute("class", "quizButton")
     cardButton.textContent = question.buttonContent
-    if (question.buttonContent === "Finish Quiz") {
-        cardButton.addEventListener("click",function(){
-            renderScoreForm(card)
-        })
-    } else if (question.buttonContent === "Start Quiz"){
+    if (question.buttonContent === "Start Quiz"){
+        let timerID = startTimer()
         cardButton.addEventListener("click",function(){
             renderCard(card,questions[index])
         }) 
+        card.appendChild(cardButton)
+        return timerID
+    } else if (question.buttonContent === "Finish Quiz") {
+        cardButton.addEventListener("click",function(){
+            renderScoreForm(card)
+            clearInterval(timerID)
+        })
     } else {
         cardButton.addEventListener("click",function(){
             scoreQuestion(questions[index])
@@ -101,8 +106,6 @@ function renderButton(card,question) {
     return
 }
 
-// need function to check that question is filled out
-
 
 // function to score questions
 // this will be attached to the button event listener
@@ -110,6 +113,8 @@ function scoreQuestion(question){
     let answer =  document.querySelector("input:checked")
     if (answer.getAttribute('value') === question.solution) {
         startScore++
+    } else {
+        timeLeft-=10
     }
 }
 
@@ -145,8 +150,10 @@ function recordScore() {
     let currentScore = new Score(name=inputName,score=startScore)
     scores.push(currentScore)
     localStorage.setItem("scores",JSON.stringify(scores))
+    startScore=0
+    clearInterval()
 }
-// function to reset current score on finish
+
 
 // Need function to show the high scores page
 function renderScoreboard(card) {
@@ -157,12 +164,25 @@ function renderScoreboard(card) {
     scores.sort((a, b) => (a.score - b.score))
     // code to grab 
     for (i =0; i < scores.length; i++){
-        console.log(scores[i])
+        // console.log(scores[i])
         item = document.createElement("li")
         item.textContent = scores[i].name  + " "  + scores[i].score
         scoreBoard.appendChild(item)
     }
     card.appendChild(scoreBoard)
+}
+
+function startTimer() {
+    let timeInterval = setInterval(function() {
+        timeLeft--
+        timer.textContent = "Timer: " + timeLeft
+        if (timeLeft <= 0) {
+            recordScore()
+            renderScoreboard(card)
+        }
+    },
+    1000)
+    return timeInterval
 }
 
 
@@ -171,11 +191,12 @@ function renderScoreboard(card) {
 // will consider the welcome page a special case of question for the purposes of easy rendering
 var questions = [
     new Question(title="",question="Welcome to the quiz",choices=[],solution=null,isStart=true,buttonContent="Start Quiz"),
-    new Question(title="Question 1/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
-    new Question(title="Question 2/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
-    new Question(title="Question 3/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
+    // new Question(title="Question 1/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
+    // new Question(title="Question 2/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
+    // new Question(title="Question 3/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
     new Question(title="Question 4/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Continue"),
     new Question(title="Question 5/5",question="Hello",choices=["A","B","C","D"],solution="A",isStart=false,buttonContent="Finish Quiz")
 ]
 
-renderCard(card,questions[0]) 
+var timerID = renderCard(card,questions[0]) 
+// console.log(timerID)
